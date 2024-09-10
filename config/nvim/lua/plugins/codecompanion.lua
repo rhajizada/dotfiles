59d257bfaf212bef1a4ec1a775b6f87ec2727214
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-09-10 00:25:48 -0400

    Updated codecompanion configuration

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -22,39 +22,42 @@ return {
       },
     },
     keys = {
-      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions" },
+      { "<leader>at", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions" },
       { "<leader>ai", "<cmd>CodeCompanion<cr>", mode = { "n", "v" }, desc = "Inline Code" },
       { "<leader>av", "<cmd>CodeCompanionAdd<cr>", mode = { "v" }, desc = "Add Visual" },
-      { "<leader>at", "<cmd>CodeCompanionToggle<cr>", mode = { "n", "v" }, desc = "Toggle" },
+      { "<leader>aa", "<cmd>CodeCompanionToggle<cr>", mode = { "n", "v" }, desc = "Toggle" },
       { "<leader>am", desc = "Switch Mode Chat" },
     },
     config = function()
       require("codecompanion").setup({
-        adapters = {
-          ollama = require("codecompanion.adapters").use("ollama", {
-            schema = {
-              model = {
-                default = "codegeex4:9b",
-              },
-              num_ctx = {
-                default = 16384,
-              },
-              num_predict = {
-                default = -1,
-              },
-            },
-          }),
-        },
         strategies = {
           chat = {
-            adapter = "ollama",
+            adapter = "codegeex4",
           },
           inline = {
-            adapter = "ollama",
+            adapter = "codegeex4",
           },
           agent = {
-            adapter = "ollama",
+            adapter = "codegeex4",
           },
+        },
+        adapters = {
+          codegeex4 = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              name = "codegeex4",
+              schema = {
+                model = {
+                  default = "codegeex4:9b",
+                },
+                num_ctx = {
+                  default = 16384,
+                },
+                num_predict = {
+                  default = -1,
+                },
+              },
+            })
+          end,
         },
       })
     end,
