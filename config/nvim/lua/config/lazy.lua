b0597cb2b0e65834f71a8b5b57abd620bb1d08c3
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-10-14 22:13:47 -0400

    Resetting back to github theme

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -51,7 +51,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catpuccin" } },
+  install = { colorscheme = { "github_dark_default" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,26 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
+    "projekt0n/github-nvim-theme",
     config = function()
-      require("nightfox").setup({
+      require("github-theme").setup({
         options = {
           transparent = true,
           styles = {
             comments = "italic",
             keywords = "bold",
-            functions = "italic,bold",
+            types = "italic,bold",
           },
-        },
-        palettes = {
-          carbonfox = {
-            bg0 = "#000000",
-            bg1 = "#000000",
-            bg2 = "#000000",
-            bg3 = "#000000",
-            bg4 = "#000000",
-            comment = "#60728a",
-          },
-        },
-        groups = {
-          carbonfox = {
-            Normal = { bg = "NONE" },
-            NormalNC = { bg = "NONE" },
-            SignColumn = { bg = "NONE" },
-            VertSplit = { bg = "NONE" },
-            EndOfBuffer = { bg = "NONE" },
+          inverse = {
+            visual = true,
           },
         },
       })
-      vim.cmd("colorscheme carbonfox")
     end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_high_contrast",
+    },
   },
 }
