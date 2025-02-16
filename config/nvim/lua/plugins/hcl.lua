d7c45bb0324bf9435d2b362d4ee75ace16307d25
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-02-16 00:20:43 -0500

    lazyvim updates, some tf stuff + task file config for yaml

---
+++ config/nvim/lua/plugins/hcl.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        tf = { "tfmt" },
+        terraform = { "tfmt" },
+        hcl = { "tfmt" },
+      },
+      formatters = {
+        tfmt = {
+          -- Specify the command and its arguments for formatting
+          command = "terraform",
+          args = { "fmt", "-" },
+          stdin = true,
+        },
+      },
+    },
+  },
+  {
+    "nathom/filetype.nvim",
+    config = function()
+      -- Setup overrides for file extensions
+      require("filetype").setup({
+        overrides = {
+          extensions = {
+            tf = "terraform",
+            tfvars = "terraform",
+            tfstate = "json",
+          },
+        },
+      })
+    end,
+  },
+}
