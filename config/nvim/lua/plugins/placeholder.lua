f626172000c4fc4b212d786947414d7c40286acf
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-05-23 02:20:22 -0400

    screenkey, lazyvim updates updated placeholder

--- config/nvim/lua/plugins/placeholder.lua
+++ config/nvim/lua/plugins/placeholder.lua
@@ -4,7 +4,35 @@ return {
     config = function()
       require("placeholder").setup({
         keymap = "<leader>cj",
-        console = "internalConsole",
+        dap_config_types = {
+          go = {
+            debugger = "go",
+          },
+          python = {
+            debugger = "debugpy",
+          },
+          lua = {
+            debugger = "nlua",
+          },
+          javascript = {
+            debugger = "pwa-node",
+            request = "launch",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            args = {},
+            env = { NODE_ENV = "development" },
+          },
+          typescript = {
+            debugger = "pwa-node",
+            request = "launch",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            args = {},
+            env = { NODE_ENV = "development" },
+          },
+        },
       })
     end,
   },
