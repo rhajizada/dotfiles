6eb0ccd9a13447865df9b39b0bec84ab6bcf5919
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-05-05 22:22:15 -0400

    lazyvim update

--- config/nvim/lua/plugins/yaml.lua
+++ config/nvim/lua/plugins/yaml.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "yaml" })
-      end
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     dependencies = {
       "b0o/SchemaStore.nvim",
