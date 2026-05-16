30bf441bee5d09da897d405edfbb3909068a1714
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-05-16 16:07:27 -0400

    fix keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,17 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Key mappings for goto-breakpoints
-local bkps = require("goto-breakpoints")
-vim.keymap.set("n", "<leader>bk", bkps.prev, { desc = "Previous Breakpoint", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>bj", bkps.next, { desc = "Next Breakpoint", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>bs", bkps.stopped, { desc = "Stopped Breakpoint", noremap = true, silent = true })
+-- Defer plugin requires until keypress so startup doesn't force-load DAP dependencies.
+vim.keymap.set("n", "<leader>bk", function()
+  require("goto-breakpoints").prev()
+end, { desc = "Previous Breakpoint", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bj", function()
+  require("goto-breakpoints").next()
+end, { desc = "Next Breakpoint", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bs", function()
+  require("goto-breakpoints").stopped()
+end, { desc = "Stopped Breakpoint", noremap = true, silent = true })
 
 -- Key mappings for snacks
-local snacks = require("snacks")
-vim.keymap.set("n", "<leader>gT", snacks.picker.git_diff, { desc = "Git Diff (Hunks)", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gT", function()
+  require("snacks").picker.git_diff()
+end, { desc = "Git Diff (Hunks)", noremap = true, silent = true })
 
 -- Key mappings for micropython_nvim
-local mp = require("micropython_nvim")
-vim.keymap.set("n", "<leader>mr", mp.run, { desc = "Run MicroPython", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>mi", mp.init, { desc = "Init project", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>mr", function()
+  require("micropython_nvim").run()
+end, { desc = "Run MicroPython", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>mi", function()
+  require("micropython_nvim").init()
+end, { desc = "Init project", noremap = true, silent = true })
